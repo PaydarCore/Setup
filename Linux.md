e64c7b3f40f9e4f91ef40526f46f4112f1014304
Alireza Hosseinitabar <alireza@hosseinitabar.com>
2022-07-29 17:25:36 +0430

    Script

---
+++ Linux.md
@@ -0,0 +1,159 @@
+<details>
+  <summary>OS</summary>
+  
+
+- Ubuntu
+  - Only 22.04 LTS
+  - Only from ubunto.com
+  - Bootable image
+    - If you're on Windows, X
+    - If you're on Linux, only using [balenaEtcher](https://ubuntu.com/tutorials/install-ubuntu-desktop#3-create-a-bootable-usb-stick)
+  - Boot
+  - Choose **Install Ubuntu**
+    - Select English (US)
+  - Keyboard layout
+    - Enlish (US)
+  - DO NOT Connect to WiFi
+    - Reason: faster installation and async update after installation
+  - Minimal installation
+    - Make sure no box is selected
+  - Erase disk and install Ubuntu
+    - Always be ready to lose your machine
+    - Advanced features
+      - None
+  - Press **Install Now**
+    - Press **Continue**
+  - Where are you?
+    - Choose Tehran
+    - Press **Continue**
+  - Computer name
+    - PaydarYourNamePc or PaydarYourNameLaptop
+    - Set password to 100 (you will be asked for it frequently)
+    - **DO NOT** choose *Log in automatically*
+    - **CHOOSE** *Require my password to login*
+  - Wait for installation
+  - **DO NOT** help Ubuntu
+    - Select **No, don't send system info**
+  - Privacy off
+  - Click **Done** (don't worry about progress)
+  - Remove everything from the left bar except the recycle bin and files
+  - Change culture
+    - Settings
+    - Region & Language
+    - Format
+    - United States
+  - Connect to WiFi
+  - Install Persian language
+    - **DO NOT** install any languages other than English & Persian
+        - **DO NOT** change defaults
+  - Confiture the keyboard
+    - Settings
+      - Accessibility
+      - Typing
+      - Repeat keys
+      - Delay = almost 10%
+      - Speed = almost 60%
+  
+</details>
+
+---
+
+- Git 
+  - Generate keys and add your public key to GitHub 
+    - Open terminal 
+    - ssh-keygen -t ed25519 -C "your-github-email" 
+    - Enter 3 times 
+      - Accept default filename 
+      - Empty password 
+      - Empty password, again 
+    - GitHub 
+      - Settings 
+      - SSH and GPG keys 
+      - New SSH Key 
+      - Copy/paste your public key there 
+        - Using Files 
+        - Go to the home folder 
+        - Show hidden files 
+        - .ssh folder 
+        - id_ed25519.pub 
+        - Right-click, open with the Text Editor, then copy 
+  - Introduce yourself to git 
+    - `git config --global user.email "your-email-of-github-here"` 
+    - `git config --global user.name "your name here"`
+  - Login into GitHub Container Registry
+    - `docker login ghcr.io`
+    - username
+    - package reading personal access token
+
+- Install VPN and connect to it (in case of doubt, ASK)
+- Update Ubuntu (using VPN)
+  - Settings
+  - Software Update
+- Install software using script
+  - `sudo mkdir /Temp`
+  - `sudo chmod 777 /Temp`
+  - `cd /Temp`
+  - `wget https://raw.githubusercontent.com/HolismHolding/Infra/main/Setup/DevelopmentEnvironment.sh`
+  - `sudo chmod 777 DevelopmentEnvironment.sh`
+  - `sudo ./DevelopmentEnvironment.sh`
+
+- **DO NOT** install any other extension on VS Code
+  - In case you need something, talk to the team
+
+
+- Wireshark
+  - `sudo add-apt-repository universe`
+  - `sudo apt install wireshark`
+  - Hit `Yes`
+  - `sudo dpkg-reconfigure wireshark-common`
+  - `sudo chmod +x /usr/bin/dumpcap`
+  - Add this to `~/.bashrc`
+  - `export SSLKEYLOGFILE=~/.ssl-key.log`
+  - `touch ~/.ssl-key.log`
+- Docker
+  - Login
+    - `docker login`
+  - In case of permission denied
+    - test with `sudo` and make sure it works
+    - if it works, run these commands
+      - `sudo gpasswd -a $USER docker`
+      - `newgrp docker`
+      - `sudo groupadd docker`
+      - `sudo usermod -aG docker ${USER}`
+      - `sudo usermod -aG docker $USER`
+
+- Configuration
+  - Add these to favorites, in order
+    - Files
+    - Chrome
+    - VS Code
+    - Terminal
+    - Editor
+- Chrome
+  - Make default
+    - On the first run, it asks for it
+  - Do not send crash reports
+  - Sign in
+  - Extensions
+    - JSONView
+    - Grammarly
+    - React Developer Tools
+- VS Code
+  - Turn on sync (using GitHub)
+  - Word wrap
+    - Files => Preferences => Settings
+    - Search for “word wrap”
+    - Select “on” from the dropdown
+- Root
+  - `sudo mkdir -p /root/.ssh`
+  - `sudo ln -f -s ~/.ssh/id_ed25519 /root/.ssh/id_ed25519`
+  - `sudo ln -f -s ~/.ssh/id_ed25519.pub /root/.ssh/id_ed25519.pub`
+  - `sudo ln -f -s ~/.ssh/known_hosts /root/.ssh/known_hosts`
+
+* Development
+  - For each company (top-level URL path segment in GitHub for example), create a directory in / (root). For example,  
+    `mkdir /HolismHolding`.
+    - Make sure the name maps the URL segment or the company name
+  - Give that folder full permission so that all applications would work without bothering your development with `sudo` stuff:  
+    `sudo chmod -R 777 /HolismHolding`
+  - Only [use SSH to connect to GitHub](https://www.freecodecamp.org/news/how-to-fix-git-always-asking-for-user-credentials/).
---
+++ Linux.sh
@@ -0,0 +1,245 @@
+#!/bin/bash
+
+function Write()
+{
+    sudo echo ""
+    sudo echo "----------"
+    sudo echo ""
+    sudo echo $1
+    sudo echo ""
+    sudo echo "----------"
+    sudo echo ""
+}
+
+function InstallChrome()
+{
+    Write "Installing Google Chrome ..."
+
+    sudo apt-get update 
+    #sudo apt upgrade
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+    Write "Installed Google Chrome"
+}
+
+function InstallVsCode()
+{
+    Write "Installing VS Code ..."
+
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    rm -f packages.microsoft.gpg
+
+    sudo apt install apt-transport-https
+    sudo apt update
+    sudo apt install code -y
+
+    code --install-extension ms-vscode-remote.remote-containers
+
+    Write "Installed VS Code"
+}
+
+function InstallGit()
+{
+    Write "Installing Git ..."
+
+    sudo apt-get install git -y
+
+    Write "Installed Git"
+}
+
+function InstallDocker()
+{
+    Write "Installing Docker ..."
+
+    sudo apt-get update
+
+    sudo apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release -y
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+    sudo docker run hello-world
+    
+    # sudo gpasswd -a $USER docker
+    # newgrp docker
+    # sudo groupadd docker
+    # sudo usermod -aG docker ${USER}
+    # sudo usermod -aG docker $USER
+
+    Write "Installed Docker"
+}
+
+function InstallDockerCompose()
+{
+    Write "Installing Docker Compose ..."
+
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+    sudo chmod +x /usr/local/bin/docker-compose
+    docker-compose --version
+
+    Write "Installed Docker Compose"
+}
+
+function InstallAnydesk()
+{
+    Write "Installing AnyDesk ..."
+
+    wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+    echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+    sudo apt update
+    sudo apt install anydesk -y
+
+    Write "Installed AnyDesk"
+}
+
+function InstallNginx()
+{
+    Write "Installing Nginx ..."
+
+    sudo apt install nginx -y
+    sudo nginx -v
+
+    Write "Installed Nginx"
+}
+
+function InstallMkcert()
+{
+    Write "Installing Mkcert ..."
+
+    sudo apt install libnss3-tools
+    wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64
+    sudo cp mkcert-v1.4.3-linux-amd64 /usr/local/bin/mkcert
+    sudo chmod +x /usr/local/bin/mkcert
+    mkcert -install
+
+    Write "Installed Mkcert"
+}
+
+function InstallMicro()
+{
+    Write "Installing Micro ..."
+
+    sudo chmod -R 777 /usr/local/bin
+    cd /usr/local/bin
+    sudo curl https://getmic.ro | bash
+
+    # or
+    # wget https://github.com/zyedidia/micro/releases/download/v2.0.10/micro-2.0.10-amd64.deb
+    # sudo apt install ./micro-2.0.10-amd64.deb
+
+    Write "Installed Micro"
+}
+
+function InstallTelnet()
+{
+    Write "Installing Telnet ..."
+
+    sudo apt-get install telnet -y
+
+    Write "Installed Telnet"
+}
+
+function InstallBeyondCompare()
+{
+    Write "Installing Beyond Compare ..."
+
+    wget https://www.scootersoftware.com/bcompare-4.4.2.26348_amd64.deb
+    sudo apt-get update
+    sudo apt-get install gdebi-core -y
+    sudo gdebi -n bcompare-4.4.2.26348_amd64.deb
+
+    Write "Installed Beyond Compare"
+}
+
+function InstallJq()
+{
+    Write "Installing jq ..."
+
+    sudo apt-get update
+    sudo apt-get install jq -y
+
+    Write "Installed jq"
+}
+
+function RegisterPaydarCommands()
+{
+    Write "Registering Paydar Commands ..."
+
+    sudo chmod 777 /etc/bash.bashrc
+    sudo echo 'PATH="${PATH}:/PaydarHolding/Infra/Commands"' >> /etc/bash.bashrc
+
+    Write "Registered Paydar Commands"
+}
+
+function InstallVpn()
+{
+    Write "Creating VPN ..."
+    # sudo apt install openconnect
+    # micro /Vpn
+    # printf 'username\npassword' | sudo openconnect vpn-server-address
+}
+
+function SetDockerPermissions()
+{
+    Write "Setting docker permissions ... "
+    
+    sudo gpasswd -a $USER docker
+    newgrp docker
+    sudo groupadd docker
+    sudo usermod -aG docker ${USER}
+    sudo usermod -aG docker $USER
+    
+    Write "Set docker permissions"
+}
+
+function DownloadVsCodeExtensions()
+{
+    Write "Downloading VS Code extensions ... "
+
+    sudo mkdir -p /PaydarHolding/Extensions
+    wget https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-dotnettools/vsextensions/csharp/1.25.0/vspackage?targetPlatform=linux-x64
+    mv 'vspackage?targetPlatform=linux-x64' /PaydarHolding/Extensions/CSharp.vsix
+
+    Write "Downloaded VS Code extensions "
+}
+
+Write "Paydar Holding Installation"
+
+InstallChrome
+InstallVsCode
+InstallGit
+InstallDocker
+InstallDockerCompose
+# InstallDotNet
+# InstallNode
+# InstallSqlServer
+# InstallAzureDataStudio
+InstallAnydesk
+InstallNginx
+InstallMkcert
+InstallMicro
+InstallTelnet
+InstallBeyondCompare
+InstallJq
+DownloadVsCodeExtensions
+RegisterPaydarCommands
+
+sudo apt install rename
+sudo apt-get install -y baobab
+
+Write "IMPORTANT => RESTART YOUR SYSTEM"
+SetDockerPermissions
